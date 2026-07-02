58b0999a4a5c479dc0c32ba42bc5358e2864c764
nuriev_t_r <tim.nur.usa@gmail.com>
2026-07-02 19:27:00 +0300

    junior_api_test: добавлены проверки

--- src/test/java/iteration_2/UserChangeNameTest.java
+++ src/test/java/iteration_2/UserChangeNameTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class UserChangeNameTest {
     final String AUTH = "Basic dGVzdFVzZXIxOnRlc3RVc2VyMSQ=";
@@ -37,7 +38,8 @@ public class UserChangeNameTest {
                 .put("/customer/profile")
                 .then()
                 .assertThat()
-                .statusCode(HttpStatus.SC_OK);
+                .statusCode(HttpStatus.SC_OK)
+                .body("customer.name", equalTo("New Name"));
     }
 
     @ParameterizedTest
@@ -55,6 +57,7 @@ public class UserChangeNameTest {
                 .put("/customer/profile")
                 .then()
                 .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(equalTo("Name must contain two words with letters only"));
     }
 }
