d6985b668e4087c73d27a88fd7bf1c224e881a48
nuriev_t_r <tim.nur.usa@gmail.com>
2026-07-03 00:26:46 +0300

    middle_api_test: переписал UserTransferTest

--- src/test/java/iteration_2/UserTransferTest.java
+++ src/test/java/iteration_2/UserTransferTest.java
@@ -1,125 +1,82 @@
 package iteration_2;
 
-import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
-import io.restassured.http.ContentType;
-import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.BeforeAll;
+import models.TransferRequest;
 import org.junit.jupiter.api.Test;
-import org.json.JSONObject;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import requests.TransferRequester;
+import specs.RequestSpecs;
+import specs.ResponseSpec;
 
-import java.util.List;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import java.util.stream.Stream;
 
 public class UserTransferTest {
-    final String AUTH = "Basic dGVzdFVzZXIxOnRlc3RVc2VyMSQ=";
-
-    @BeforeAll
-    public static void setupRestAssured(){
-        RestAssured.filters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter()));
-    }
 
     @ParameterizedTest
     @ValueSource(doubles = {0.01, 10000, 500})
     public void userCanTransferBetweenTheirAccounts(double amount) {
-        RestAssured.baseURI = "http://localhost:4111";
-        JSONObject requestBody = new JSONObject()
-                .put("amount", amount)
-                .put("receiverAccountId", 2)
-                .put("senderAccountId", 1);
+        TransferRequest transferRequest = TransferRequest.builder()
+                .amount(amount)
+                .senderAccountId(1)
+                .receiverAccountId(2)
+                .build();
 
-        JSONObject requestBodyRevert = new JSONObject()
-                .put("amount", amount)
-                .put("receiverAccountId", 1)
-                .put("senderAccountId", 2);
+        new TransferRequester(
+                RequestSpecs.userSpec(),
+                ResponseSpec.requestReturnsOK("message", "Transfer successful"))
+                .post(transferRequest);
+    }
 
-        given()
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .header("Authorization", AUTH)
-                .body(requestBody.toString())
-                .when()
-                .post("/api/v1/accounts/transfer")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .body("message", equalTo("Transfer successful"))
-                .body("amount", equalTo((float) amount));
-
-        given()
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .header("Authorization", AUTH)
-                .body(requestBodyRevert.toString())
-                .when()
-                .post("/accounts/transfer");
+    public static Stream<Arguments> invalidAmount() {
+        return Stream.of(
+                Arguments.of(0, "Transfer amount must be at least 0.01"),
+                Arguments.of(10000.1, "Transfer amount cannot exceed 10000")
+        );
     }
 
     @ParameterizedTest
-    @ValueSource(doubles = {0, 10001})
-    public void userCannotTransferInadmissibleAmountBetweenTheirAccounts(double amount) {
-        RestAssured.baseURI = "http://localhost:4111/api/v1";
-        JSONObject requestBody = new JSONObject()
-                .put("amount", amount)
-                .put("receiverAccountId", 2)
-                .put("senderAccountId", 1);
+    @MethodSource("invalidAmount")
+    public void userCannotTransferInadmissibleAmountBetweenTheirAccounts(double amount, String errorMessage) {
+        TransferRequest transferRequest = TransferRequest.builder()
+                .amount(amount)
+                .senderAccountId(1)
+                .receiverAccountId(2)
+                .build();
 
-        given()
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .header("Authorization", AUTH)
-                .body(requestBody.toString())
-                .when()
-                .post("/accounts/transfer")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST);
+        new TransferRequester(
+                RequestSpecs.userSpec(),
+                ResponseSpec.requestReturnsBadRequest(errorMessage))
+                .post(transferRequest);
     }
 
     @Test
     public void userHasNotEnoughAmountToTransfer() {
-        RestAssured.baseURI = "http://localhost:4111/api/v1";
-        JSONObject requestBody = new JSONObject()
-                .put("amount", 5000)
-                .put("receiverAccountId", 1)
-                .put("senderAccountId", 2);
+        TransferRequest transferRequest = TransferRequest.builder()
+                .amount(5000)
+                .senderAccountId(1)
+                .receiverAccountId(2)
+                .build();
 
-        given()
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .header("Authorization", AUTH)
-                .body(requestBody.toString())
-                .when()
-                .post("/accounts/transfer")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_BAD_REQUEST)
-                .body(equalTo("Invalid transfer: insufficient funds or invalid accounts"));
+        new TransferRequester(
+                RequestSpecs.userSpec(),
+                ResponseSpec.requestReturnsBadRequest("Invalid transfer: insufficient funds or invalid accounts"))
+                .post(transferRequest);
     }
 
     @Test
     public void userCanTransferToAnotherUser() {
-        RestAssured.baseURI = "http://localhost:4111/api/v1";
-        JSONObject requestBody = new JSONObject()
-                .put("amount", 100)
-                .put("receiverAccountId", 3)
-                .put("senderAccountId", 1);
+        TransferRequest transferRequest = TransferRequest.builder()
+                .amount(100)
+                .senderAccountId(1)
+                .receiverAccountId(3)
+                .build();
 
-        given()
-                .accept(ContentType.JSON)
-                .contentType(ContentType.JSON)
-                .header("Authorization", AUTH)
-                .body(requestBody.toString())
-                .when()
-                .post("/accounts/transfer")
-                .then()
-                .assertThat()
-                .statusCode(HttpStatus.SC_OK)
-                .body("message", equalTo("Transfer successful"));
+        new TransferRequester(
+                RequestSpecs.userSpec(),
+                ResponseSpec.requestReturnsOK("message", "Transfer successful"))
+                .post(transferRequest);
+
     }
 }
